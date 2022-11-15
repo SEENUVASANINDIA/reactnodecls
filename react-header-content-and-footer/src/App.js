e97ec5288afe4fe3b24493853b042a329e587bea
SEENUVASAN <95583618+SEENUVASANINDIA@users.noreply.github.com>
2022-11-15 05:57:39 +0530

    react header and footer

---
+++ react-header-content-and-footer/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header title="My header" subtitle="subtitle2" />
+      <Main message="My content" />
+      <Footer note="Footer Note" />
+    </div>
+  );
+}
